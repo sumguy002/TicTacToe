4c5355887e8ea1ba05072ea2eadff83f6e479e21
sumguy002 <45304746+sumguy002@users.noreply.github.com>
2019-05-08 08:57:48 -0700

    Implemented SquareBoard with 2D array

--- src/com/company/Squareboard.java
+++ src/com/company/Squareboard.java
@@ -1,4 +1,9 @@
 package com.company;
 
 public class Squareboard {
+    //implementation as a 2d array of ints
+    private int[][] board;
+    public Squareboard(int dimensions){
+        board = new int[dimensions][dimensions];
+    }
 }
