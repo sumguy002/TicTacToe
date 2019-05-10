5abfbfae5e33f35e7d686c1542db36cae87fd2d2
sumguy002 <45304746+sumguy002@users.noreply.github.com>
2019-05-10 07:43:33 -0700

    completed SquareBoard toString method Added default 3x3 constructor to Squareboard

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Goodbyeworld");
+        Squareboard bob = new Squareboard();
+        System.out.println(bob);
     }
 }
--- src/com/company/Squareboard.java
+++ src/com/company/Squareboard.java
@@ -1,15 +1,21 @@
 package com.company;
 
 public class Squareboard {
-    //implementation as a 2d array of ints
+    //implementation as a 2d array of boards
     private Square[][] board;
     private int winLen; //winLen is the number of things in a row that is needed
+    public Squareboard(){ //defaults to a 3 by 3
+        board = new Square[3][3];
+        winLen = 3;
+
+    }
     public Squareboard(int dimensions, int winCon){
         board = new Square[dimensions][dimensions];
         winLen = winCon;
 
     }
-    public int checkRowWin(){ // maybe this foreach loop isn't the best idea
+
+    private int checkRowWin(){ // maybe this foreach loop isn't the best idea
         // 0 means no winner, 1 means player 1 wins, 2 means player 2 wins
         int consecutive = 0;
         int curID = 0; //the number ID of the last one tested
@@ -27,7 +33,7 @@ public class Squareboard {
         }
         return 0;
     }
-    public int checkColWin(){
+    private int checkColWin(){
         // 0 means no winner, 1 means player 1 wins, 2 means player 2 wins
         int consecutive = 0;
         int curID = 0; //the number ID of the last one tested
@@ -46,15 +52,9 @@ public class Squareboard {
         }
         return 0;
     }
-    public int checkDiagonalWin(){
+    private int checkDiagonalWin(){
         /*
-        how to do this? I'm thinking iterating through every single edge block and having a loop to search through the two diagonals.
-
-        That, or I could try some complicated recursive implementation where I have edges and nodes? meh too hard to wrap head around.
-
-        honestly I think that first one is the only way to do it? let's give it a go. Either that or we iterate
-        through every single node and do a search. Seems really inefficient though so I don't know if that would be smart at all
-        In eed to search through two sides though. And each side I have to search up, then search sound
+        assumes squareboard. A rectangle would be even worse tbh
          */
         int consecutive = 0;
         int curID = 0; //the number ID of the last one tested
@@ -113,10 +113,24 @@ public class Squareboard {
         }
         return 0;
     }
-    public boolean checkWin(){
-        for (int i = 0; i < 3; i++){
+    public int checkWin(){
+        if (checkColWin() != 0) return checkColWin();
+        if (checkRowWin() != 0) return checkRowWin();
+        if (checkDiagonalWin() != 0) return checkDiagonalWin();
+        return 0;
+    }
+    public String toString(){
+        String ret = "";
+        for (Square[] row: board){
+            for (Square col: row){
+                if (col.getFace() == 0) ret += "_";
+                else if (col.getFace() == 1) ret += "O";
+                else if (col.getFace() == 2) ret += "X";
 
+            }
+            ret +=  "\n";
         }
+        return ret;
     }
 
 }
